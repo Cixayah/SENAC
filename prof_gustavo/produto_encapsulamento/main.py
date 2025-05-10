2d7fab131f3f5c658d8196371f4bef0fd1a62dd2
Cix <gabrielviniciodacosta@gmail.com>
2025-05-10 10:45:51 -0300

    #

---
+++ prof_gustavo/produto_encapsulamento/main.py
@@ -0,0 +1,6 @@
+from classe import Produto
+notebook = Produto ('Notebook Dell i7', 3800, 5800)
+print('Mudar preço para 1000')
+notebook.preco_venda=1000
+print('Mudar preço para 5100')
+notebook.preco_venda=5100
